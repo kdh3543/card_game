c66de6f5250ea127f8d5070956772749360a46f0
kdh3543 <rlaehdgus3543@naver.com>
2023-02-28 19:07:47 +0900

    Fix: card_game 작업중

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,12 +1,37 @@
 import styles from "@/styles/Game.module.css";
+import { useState } from "react";
 
 function Game() {
+  const [show, setShow] = useState(Array.from({ length: 6 }, () => false));
+  const [test, setTest] = useState<any>();
+  console.log({ ...show });
+  const showCard = (e: any) => {
+    // setTest(index);
+    console.log(e.target);
+    // console.log(index);
+    // setShow(!show[index]);
+  };
+
+  const arr = Array.from({ length: 6 }, (_, i) => i);
   return (
     <>
       <main className={styles.main}>
         <div className={styles.cardContainer}>
-          <div className={styles.card}>
-            <div className={styles.cardContent}>1</div>
+          {arr.map((value, index) => (
+            <div className={styles.card} key={value}>
+              <div className={styles.cardContent} onClick={(e) => showCard(e)}>
+                <text className={show ? styles.cardShow : styles.cardHide}>
+                  {value}
+                </text>
+              </div>
+            </div>
+          ))}
+          {/* <div className={styles.card}>
+            <div className={styles.cardContent} onClick={showCard}>
+              <text className={show ? styles.cardShow : styles.cardHide}>
+                1
+              </text>
+            </div>
           </div>
           <div className={styles.card}>
             <div className={styles.cardContent}>2</div>
@@ -22,7 +47,7 @@ function Game() {
           </div>
           <div className={styles.card}>
             <div className={styles.cardContent}>6</div>
-          </div>
+          </div> */}
         </div>
       </main>
     </>
