94201e4d213f9e3256ae2ee095eafe8e435175ff
kdh3543 <rlaehdgus3543@naver.com>
2023-03-03 17:43:03 +0900

    Fix: 홈 화면 및 단계 설정, 성공 실패 판단 작업 중

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,21 +1,20 @@
 import styles from "@/styles/Game.module.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 function Game() {
   const [cardArray, setCardArray] = useState<number[]>([]);
   const [answerArray, setAnswerArray] = useState<number[]>([]);
+  const [boolArray, setBoolArray] = useState<number[]>([]);
   const [flip, setFlip] = useState<boolean[]>([]);
   const [cardCount, setCardCount] = useState(0);
   const [answer, setAnswer] = useState<number[]>([]);
   const [answerCount, setAnswerCount] = useState(0);
-  const [time, setTime] = useState(3600);
-
-  useEffect(() => {
-    const date = new Date(3600);
-    console.log(date);
-    setTimeout(() => {}, 1);
-  }, [time]);
-
+  const [time, setTime] = useState(5);
+  const [success, setSuccess] = useState("");
+  const router = useRouter();
+  let initNum: any = router.query.level;
   const choiceCard = (index: number, value: number) => {
     setAnswer([...answer, value]);
 
@@ -23,49 +22,93 @@ function Game() {
     tempArr[index] = true;
     setCardCount(cardCount + 1);
     setFlip([...tempArr]);
-    console.log(answer);
   };
 
   useEffect(() => {
     if (cardCount === 2) {
-      console.log(answer);
-      // setAnswer([]);
       if (answer[answer.length - 1] === answer[answer.length - 2]) {
+        setAnswerArray([...answerArray, answer[answer.length - 1]]);
         setAnswerCount(answerCount + 1);
       } else {
-        console.log(flip);
         setTimeout(() => {
-          setFlip(Array.from({ length: 6 }, () => false));
+          if (boolArray.length === 0) {
+            setFlip(Array.from({ length: initNum }, () => false));
+          }
+          let tempArr = flip;
+          for (let i = 0; i < boolArray.length; i++) {
+            if (boolArray[i] === -1) {
+              tempArr[i] = false;
+            } else {
+              tempArr[i] = true;
+            }
+
+            setFlip([...tempArr]);
+          }
         }, 100);
       }
       setCardCount(0);
     }
   }, [cardCount]);
 
-  console.log(flip);
   useEffect(() => {
-    setFlip(Array.from({ length: 6 }, () => true));
+    const num = cardArray.map((value, index) => {
+      if (answerArray.includes(value)) return index;
+      return -1;
+    });
+
+    setBoolArray(num);
+  }, [answerArray]);
+  useEffect(() => {
+    if (!router.query.level) return;
+    setFlip(Array.from({ length: initNum }, () => true));
     setTimeout(() => {
-      setFlip(Array.from({ length: 6 }, () => false));
+      setFlip(Array.from({ length: initNum }, () => false));
     }, 1000);
     setCardArray(
-      Array.from({ length: 6 }, (_, i) => Math.ceil((i + 1) / 2)).sort(
+      Array.from({ length: initNum }, (_, i) => Math.ceil((i + 1) / 2)).sort(
         () => Math.random() - 0.5
       )
     );
+  }, [router?.query?.level]);
 
-    console.log(cardArray);
-  }, []);
-
+  // 시간 초과
+  useEffect(() => {
+    if (time <= 0) {
+      setSuccess("FAILED!!");
+      return;
+    }
+    setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+    console.log(answerArray);
+  }, [time]);
   return (
     <>
+      <Head>
+        <title>카드 맞추기</title>
+      </Head>
       <main className={styles.main}>
         <div className={styles.answerCount}>
-          {"ANSWER COUNT:"} {time}
+          {"REMAIN TIME:"} {time}
         </div>
+        {time === 0 && answerArray.length !== parseInt(initNum) / 2 ? (
+          <div className={styles.fail}>{success}</div>
+        ) : (
+          <div className={styles.success}>{success}</div>
+        )}
+
         <div className={styles.cardContainer}>
           {cardArray.map((value: number, index: number) => (
-            <div className={styles.card} key={index}>
+            <div
+              className={
+                initNum === "8"
+                  ? styles.easy
+                  : initNum === "12"
+                  ? styles.middle
+                  : styles.hard
+              }
+              key={index}
+            >
               <div
                 className={
                   flip[index] ? styles.cardContent : styles.cardCheckedContent
