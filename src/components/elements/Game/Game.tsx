aac17cf03f15e91f9f0e7904a3c2d8b4c72b8f1c
kdh3543 <rlaehdgus3543@naver.com>
2023-03-16 21:30:46 +0900

    Fix: card 이미지 효과 수정

--- src/components/elements/Game/Game.tsx
+++ src/components/elements/Game/Game.tsx
@@ -24,6 +24,7 @@ const Count = styled.div`
   font-size: 20px;
   font-weight: bold;
   font-family: "Manrope";
+  color: black;
 `;
 
 const Result = styled.div`
@@ -68,20 +69,55 @@ const Level = styled.div`
   padding: 10px;
 `;
 
-const Content = styled.div<StylePropsType>`
+// const Content = styled.div<StylePropsType>`
+//   width: 200px;
+//   height: 200px;
+//   background-color: gray;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   cursor: ${(props) => props.cursor};
+//   border-radius: 15px;
+//   position: relative;
+// `;
+
+// const Card = styled.div<StylePropsType>`
+//   visibility: ${(props) => props.visibility};
+// `;
+
+const CardContainer = styled.div<{ flipped: boolean }>`
+  position: relative;
   width: 200px;
   height: 200px;
-  background-color: gray;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: ${(props) => props.cursor};
-  border-radius: 15px;
-  position: relative;
+
+  transition: all 0.5s;
+  transform-style: preserve-3d;
+  & > div {
+    backface-visibility: hidden;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    border-radius: 15px;
+  }
+  transform: ${({ flipped }) =>
+    flipped ? "rotateY(180deg)" : "rotateY(0deg)"};
 `;
 
-const Card = styled.div<StylePropsType>`
-  visibility: ${(props) => props.visibility};
+const CardFront = styled.div<{ flipped: boolean }>`
+  background-color: gray;
+  color: white;
+  cursor: pointer;
+`;
+
+const CardBack = styled.div<{ flipped: boolean }>`
+  color: black;
+  background-color: gray;
+  cursor: not-allowed;
+  transform: rotateY(180deg);
 `;
 function Game() {
   const [cardArray, setCardArray] = useState<number[]>([]);
@@ -93,6 +129,12 @@ function Game() {
   const [answerCount, setAnswerCount] = useState(0);
   const [time, setTime] = useState(6000);
   const [success, setSuccess] = useState("");
+
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
   const router = useRouter();
   const initNum: any = router.query.level;
   const choiceCard = (index: number, value: number) => {
@@ -114,6 +156,7 @@ function Game() {
           if (boolArray.length === 0) {
             setFlip(Array.from({ length: initNum }, () => false));
           }
+          console.log(flip);
           let tempArr = flip;
           for (let i = 0; i < boolArray.length; i++) {
             if (boolArray[i] === -1) {
@@ -124,7 +167,7 @@ function Game() {
 
             setFlip([...tempArr]);
           }
-        }, 100);
+        }, 500);
       }
       setCardCount(0);
     }
@@ -160,7 +203,7 @@ function Game() {
     }
     const remainTime = setTimeout(() => {
       setTime(time - 1);
-    }, 10);
+    }, 1000);
     if (boolArray.every((value) => value !== -1) && boolArray.length != 0) {
       setSuccess("SUCCESS!!");
       clearTimeout(remainTime);
@@ -188,9 +231,23 @@ function Game() {
         </Result>
         <Container>
           <Back onClick={() => router.push("/")}>{"GO BACK"}</Back>
+
           {cardArray.map((value: number, index: number) => (
             <Level key={index}>
-              <Content
+              <CardContainer flipped={flip[index]} onClick={handleClick}>
+                <CardFront
+                  onClick={
+                    flip[index]
+                      ? (e) => e.preventDefault()
+                      : () => choiceCard(index, value)
+                  }
+                  flipped={flip[index]}
+                >
+                  front
+                </CardFront>
+                <CardBack flipped={flip[index]}>{value}</CardBack>
+              </CardContainer>
+              {/* <Content
                 cursor={flip[index] ? "not-allowed" : "pointer"}
                 onClick={
                   flip[index]
@@ -201,7 +258,7 @@ function Game() {
                 <Card visibility={flip[index] ? "visible" : "hidden"}>
                   {value}
                 </Card>
-              </Content>
+              </Content> */}
             </Level>
           ))}
         </Container>
